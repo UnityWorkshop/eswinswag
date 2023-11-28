98d792fbd4fe70990786b8c0027ecbbb01b1c671
SzyZu <skwarek116@gmail.com>
2023-11-28 17:54:54 +0100

    feat: moved win code to GameManager

---
+++ Assets/Scripts/GameManagr.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManagr : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject message;
+    [SerializeField] GameObject targetBlock;
+    [SerializeField] TMP_Text timeTMP;
+
+    private bool win = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!win)
+        {
+            if (player.transform.position == targetBlock.transform.position)
+            {
+                message.SetActive(true);
+                player.GetComponent<Movement>().delete();
+                win = true;
+            }
+            else
+            {
+                message.SetActive(false);
+                timeTMP.GetComponent<timeCounter>().count();
+            }
+        }
+    }
+}
--- Assets/Scripts/timeCounter.cs
+++ Assets/Scripts/timeCounter.cs
@@ -6,33 +6,8 @@ using UnityEngine;
 
 public class timeCounter : MonoBehaviour
 {
-    [SerializeField] GameObject player;
-    [SerializeField] GameObject message;
-    [SerializeField] GameObject targetBlock;
-
-    private bool win = false;
-
-    // Start is called before the first frame update
-    void Start()
+    public void count()
     {
-        
-    }
-    
-    public void Update()
-    {
-        if (!win)
-        {
-            if (player.transform.position == targetBlock.transform.position)
-            {
-                message.SetActive(true);
-                player.GetComponent<Movement>().delete();
-                win = true;
-            }
-            else
-            {
-                message.SetActive(false);
-                this.GetComponent<TMP_Text>().text = Time.time.ToString("F2");
-            }
-        }
+        this.GetComponent<TMP_Text>().text = Time.time.ToString("F2");
     }
 }
