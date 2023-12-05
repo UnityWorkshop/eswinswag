37997faea2ce84e2c25f4479c6d7ce31a4dd926d
SzyZu <skwarek116@gmail.com>
2023-12-05 17:08:27 +0100

    problem: time

--- Assets/Scripts/timeCounter.cs
+++ Assets/Scripts/timeCounter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,8 +7,31 @@ using UnityEngine;
 
 public class timeCounter : MonoBehaviour
 {
+    private int minutes = 0;
+    private int seconds = 0;
     public void count()
     {
-        this.GetComponent<TMP_Text>().text = Time.time.ToString("F2");
+        //this.GetComponent<TMP_Text>().text = Time.time.ToString("F2")
+        this.GetComponent<TMP_Text>().text = time();
+    }
+
+    public void FixedUpdate()
+    {
+        if (Mathf.Ceil(Time.fixedTime) % 60 == 0)
+        {
+            minutes = minutes + 1;
+        }
+        Debug.Log("Minutes: " + minutes);
+    }
+
+    public String time()
+    {
+        float time = Mathf.Round(Time.fixedTime);
+        if (time % 60 == 0)
+        {
+            minutes = minutes + 1;
+        }
+        //Debug.Log(Time.fixedTime);
+        return minutes.ToString();
     }
 }
