b09347a7ca3ced4019f87537434cb3becc32e9c3
I-Protecc <ryanbaumeister@googlemail.com>
2023-12-19 17:17:12 +0100

    feat: SwagCounter implemented

--- Assets/Scripts/SwagOMeter.cs
+++ Assets/Scripts/SwagOMeter.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
+using DefaultNamespace;
+using TMPro;
 using UnityEngine;
 
 public class SwagOMeter : MonoBehaviour
 {
     // Start is called before the first frame update
-    /*void Start()
+
+    private TMP_Text _txt;
+    
+    private void Start()
     {
-        
+        _txt  = GetComponent<TMP_Text>() ;
+        _txt.text = "0 swags";
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
-    }*/
+        _txt.text = Swag.swagCounter + " swags";
+    }
 }
