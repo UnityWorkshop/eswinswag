0e93a2abff1f41a85292e658c4dd530af1f6c499
programertoat <leon12steam@hotmail.com>
2024-06-14 19:23:57 +0200

    fix: #31 add: reset button

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -1,5 +1,13 @@
+using System;
 using UnityEngine;
+
 public abstract class Box : MonoBehaviour
 {
+        public Vector3 startPosition;
         public abstract bool CanMove();
+
+        private void Start()
+        {
+            startPosition = transform.position;
+        }
 }
